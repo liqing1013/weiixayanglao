8da3037c46ffeb19014197fc78483beaa5ed66f6
liqing1013 <936012165@qq.com>
2019-11-30 14:35:36 +0800

    项目初始化-调整目录结构、导入vant

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './utils/register-vant.js'// 加载执行注册模块
 
 Vue.config.productionTip = false
 
---
+++ src/utils/register-vant.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'// 导入Vue
+
+import {
+  Button,
+  Cell,
+  CellGroup
+} from 'vant'// 按需导入vant
+
+Vue
+  .use(Button)
+  .use(Cell)
+  .use(CellGroup)// Vue实例各个vant组件
